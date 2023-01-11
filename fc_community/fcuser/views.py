7748624991f4ea48d73853a33485343072e4cca7
Jeongmin Lee <kyungheeyj45@naver.com>
2023-01-11 15:46:09 +0900

    email 추가

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -11,17 +11,19 @@ def register(request):
         return render(request, 'register.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
-        if not (username and password and re_password):
+        if not (username and useremail and password and re_password):
             res_data['error'] = '모든 값을 입력해야합니다.'
         elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
             fcuser.save()
