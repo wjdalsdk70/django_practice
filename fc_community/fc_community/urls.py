bc84797fca3c2be77ff4926aba2a52973f475531
Jeongmin Lee <kyungheeyj45@naver.com>
2023-01-11 15:36:47 +0900

    회원 가입 구현

--- fc_community/fc_community/urls.py
+++ fc_community/fc_community/urls.py
@@ -1,8 +1,7 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('fcuser/', include('fcuser.urls'))
+    path('fcuser/', include('fcuser.urls')),
 ]
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,7 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'reguest.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username=username,
+                password=make_password(password)
+            )
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
