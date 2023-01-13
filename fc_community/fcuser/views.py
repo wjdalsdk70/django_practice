396ee37206d0a2e4cd691bb8c1d9656475edb5fc
Jeongmin Lee <kyungheeyj45@naver.com>
2023-01-13 20:31:15 +0900

    MVT의 T 확장을 통한 상속과 Form을 활용한 로그인 페이지 만들기

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -23,23 +24,14 @@ def logout(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                request.session['user'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호를 틀렸습니다.'
-        return render(request, 'login.html', res_data)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 
 def register(request):
